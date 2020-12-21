78773a8c4b3c1ba0cec9904e0e2772e203ab88c3
Shomari Roberts <shomariroberts@gmail.com>
2020-12-20 19:06:06 -0500

    implemented an abstract class for Sorter.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0, -10, 38]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0, -10, 38]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
 const charactersCollection = new CharactersCollection('XaaybZDohsjcl');
-
-// const sorter = new Sorter(numbersCollection);
-const sorter = new Sorter(charactersCollection);
-
-sorter.sort();
-
-// console.log(numbersCollection.data);
-
+charactersCollection.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(20);
+linkedList.add(0);
+
+linkedList.sort();
+linkedList.print();
